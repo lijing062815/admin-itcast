f45f286070ce72b725ac637c4ad446ff700f2230
李不言 <lijing062815@163.com>
2018-07-20 17:13:28 +0800

    首页-侧边栏

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,30 @@
 <template>
   <div class="home">
     <el-container class="el-container">
-      <el-aside width="200px" class="el-aside">Aside</el-aside>
+      <!-- 侧边栏 -->
+      
+      <el-aside width="200px" class="el-aside">
+        <div class="logo"></div>
+         <el-menu
+      default-active="2"
+      class="el-memu-admin"
+      @open="handleOpen"
+      @close="handleClose"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b">
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>用户管理</span>
+        </template>
+          <el-menu-item index="2">
+            <i class="el-icon-menu"></i>
+            <span>用户列表</span>
+          </el-menu-item>
+      </el-submenu>
+    </el-menu>
+      </el-aside>
     <el-container>
       <el-header class="el-header">Header</el-header>
       <el-main>Main</el-main>
@@ -13,6 +36,14 @@
 <script>
 import {getUserList} from '@/api'
 export default {
+   methods: {
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath)
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath)
+      }
+    },
   mounted() {
     let params = {params:{query:'',pagenum:1,pagesize:1}}
     getUserList(params).then(res=>{
