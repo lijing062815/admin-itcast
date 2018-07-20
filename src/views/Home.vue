35feb46bc3b5c48e904619d2c78fc4a5faf89c6b
李不言 <lijing062815@163.com>
2018-07-20 16:46:18 +0800

    首页整体布局

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="home">
-    登录成功
+    <el-container class="el-container">
+      <el-aside width="200px" class="el-aside">Aside</el-aside>
+    <el-container>
+      <el-header class="el-header">Header</el-header>
+      <el-main>Main</el-main>
+  </el-container>
+</el-container>
   </div>
 </template>
 
@@ -17,5 +23,48 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+  .home {
+    height: 100%;
+    background-color: #e5e5e5;
+    .el-memu-admin:not(.el-menu--collapse) {
+      width: 200px;
+      min-height: 400px;
+    }
+    .el-container {
+      height: 100%
+    }
+    .el-aside {
+      background-color: #545c64;
+    }
+    .el-header {
+      display:flex;
+      justify-content: space-between;
+      align-items: center;
+      background-color: #009688;
+    }
+    .logo {
+      height: 60px;
+      background: url(../assets/logo.png);
+      background-size: cover;
+      background-color: #fff;
+    }
+    .toggle-btn {
+      padding: 0 10px;
+      margin-left: -20px;
+      font-size: 36px;
+      line-height: 60px;
+      color: white;
+      cursor: pointer;
+      &:hover {
+        background-color: #00635a;
+      }
+    }
+    .system-title {
+      font-size: 280px;
+      color: #fff;
+    }
+    .welcom, {
+      color: #fff
+    }
+  }
 </style>
